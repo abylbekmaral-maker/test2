fc1e2410acdf957b4f371794d4e78bb07a0088d6
Maral <abylbekmaral@gmail.com>
2026-04-22 16:06:25 +0900

    first commit

---
+++ project_prerequisite-task_2_1_3/src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld hello1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hello2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(hello1 == hello2);
+        System.out.println(cat1 == cat2);
+    }
+}
